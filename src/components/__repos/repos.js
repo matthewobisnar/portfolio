31e4c524e9a74185bd63f4b7db926ffb38d1f84b
oreohappy <matthewotarrabisnar@gmail.com>
2021-01-18 19:18:46 +0800

    timeline added

--- src/components/__repos/repos.js
+++ src/components/__repos/repos.js
@@ -1,7 +1,12 @@
+import { resizerMixins } from '../../Store/Resizer/ResizerMixins.js';
+import { myBioMixins } from '../../Store/MyBio/MyBioMixins.js';
+
 export default {
+    mixins: [resizerMixins, myBioMixins],
     data(){
         return {
             hello: "ss"
         }
     }
+    
 }
